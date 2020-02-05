d0b2f362c35281d297c1f264e42d9e96ca8e81c9
Hoang Pham <hoangpham1.4.99@gmail.com>
2020-02-05 21:52:37 +0700

    CORS

--- staffWebApp/src/main/java/com/spring2020/staffwebapp/controller/ProductController.java
+++ staffWebApp/src/main/java/com/spring2020/staffwebapp/controller/ProductController.java
@@ -29,6 +29,7 @@ public class ProductController
         return productRetrieveService.findAllProducts(pageable);
     }
 
+    @CrossOrigin(origins = "http://localhost:9000")
     @GetMapping("/details")
     public Optional<ProductDto> showProductDetails(@RequestParam(value = "Product Id") Long id)
     {
