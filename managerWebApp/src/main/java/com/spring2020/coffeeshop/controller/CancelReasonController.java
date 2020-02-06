fea2f9e5d11370aace844dd889fe29041596a06f
Tram Phan <hoang.tram2908@gmail.com>
2020-02-06 21:11:59 +0700

    add cors config global

--- managerWebApp/src/main/java/com/spring2020/coffeeshop/controller/CancelReasonController.java
+++ managerWebApp/src/main/java/com/spring2020/coffeeshop/controller/CancelReasonController.java
@@ -3,11 +3,13 @@ package com.spring2020.coffeeshop.controller;
 import com.spring2020.coffeeshop.domain.dto.CancelReasonDto;
 import com.spring2020.coffeeshop.service.CancelReasonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/cancelReasons")
-@CrossOrigin(origins = "http://localhost:3000")
 public class CancelReasonController {
 
     @Autowired
--- managerWebApp/src/main/java/com/spring2020/coffeeshop/controller/OrderController.java
+++ managerWebApp/src/main/java/com/spring2020/coffeeshop/controller/OrderController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
@@ -14,7 +17,6 @@ import static com.spring2020.coffeeshop.util.ConstantUtil.DATE_TIME_PATTERN;
 
 @RestController
 @RequestMapping("/orders")
-@CrossOrigin(origins = "http://localhost:3000")
 public class OrderController {
 
     @Autowired
